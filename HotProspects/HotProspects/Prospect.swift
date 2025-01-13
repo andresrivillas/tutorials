c5bb00ce09f1eb615af65dc9eb2363aa85e9aed0
Andres Rivillas <arivillas@enlace.org>
2025-01-13 14:56:01 -0500

    Day 82, 83

---
+++ HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,21 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Andres Rivillas on 2/10/24.
+//
+
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+}
