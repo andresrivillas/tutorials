2211fba3ae9a548f7d5b0b83d9565b188f79bff0
Andres Rivillas <arivillas@enlace.org>
2025-01-15 19:10:22 -0500

    Day 86, 87, 88, 89

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Andres Rivillas on 15/01/25.
+//
+
+import Foundation
+
+struct Card {
+    var propmt: String
+    var answer: String
+    
+    static let example = Card(propmt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Andres Rivillas on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
